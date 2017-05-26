b7a1e7b1c2b01b9b6a33c594ee930d2e238b3990
dmoiseenko <dmoiseenko@mykolab.com>
2017-05-26 16:42:07 +0300

    Run unit test on CI

--- test.sh
+++ test.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-echo "Hello!!!!";
+#!/usr/bin/env bash
+
+DOCKER_COMPOSE="docker-compose
+--project-name wenDev
+-f docker-compose.yml
+-f docker-compose.dev.yml
+-f docker-compose.logs.yml"
+
+${DOCKER_COMPOSE} build
+${DOCKER_COMPOSE} run --service-ports runner yarn run test:common
+${DOCKER_COMPOSE} down
