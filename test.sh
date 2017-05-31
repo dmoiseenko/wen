543b7083cf6f93c113f94110e8f1872d6805cab2
dmoiseenko <dmoiseenko@mykolab.com>
2017-05-31 11:05:33 +0300

    Pass unit tests

--- test.sh
+++ test.sh
@@ -3,11 +3,24 @@
 #!/usr/bin/env bash
 
 DOCKER_COMPOSE="docker-compose
---project-name wenDev
+--project-name wenTest
 -f docker-compose.yml
 -f docker-compose.dev.yml
 -f docker-compose.logs.yml"
 
 ${DOCKER_COMPOSE} build
-${DOCKER_COMPOSE} run --service-ports runner yarn run test:common
+${DOCKER_COMPOSE} run --service-ports runner yarn run test
+docker_command_status=$?
+
 ${DOCKER_COMPOSE} down -v
+cleanup_command_status=$?
+
+if [[ "$docker_command_status" -ne 0 ]] ; then
+	exit $docker_command_status
+fi
+
+if [[ "$cleanup_command_status" -ne 0 ]] ; then
+	exit $cleanup_command_status
+fi
+
+exit 0
