bdb517c5aa9005d19ce62e002f67e59aa0fb7c33
dmoiseenko <dmoiseenko@mykolab.com>
2017-05-31 11:12:57 +0300

    Add failed unit test

--- common/validation/test/password.validation.test.js
+++ common/validation/test/password.validation.test.js
@@ -7,7 +7,7 @@ describe('password.validation', () => {
   });
 
   it('should throw error if email is null', () => {
-    expect(() => validatePassword(null)).toThrow('Invalid password');
+    expect(() => validatePassword(null)).toThrow('Invalid password1');
   });
 
   it('should throw error if email is undefined', () => {
