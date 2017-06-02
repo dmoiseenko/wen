61067b8ec2280b869eeef06348683f8b97ae444e
dmoiseenko <dmoiseenko@mykolab.com>
2017-06-02 14:15:17 +0300

    Add common webpack prebuild config

--- client/main/webpack/main.prebuild.webpack.config.js
+++ client/main/webpack/main.prebuild.webpack.config.js
@@ -1,12 +1,9 @@
 const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const nodeExternals = require('webpack-node-externals');
 
-const config = require('../../../common/config.js');
+const webpackPrebuildConfig = require('../../common/webpack/webpack.prebuild.config.js');
 
 
-module.exports = {
+module.exports = webpackPrebuildConfig({
   entry: {
     html: [
       '../Main.html.jsx'
@@ -22,98 +19,5 @@ module.exports = {
     chunkFilename: '[id].js',
     libraryTarget: 'commonjs2'
   },
-  context: __dirname,
-  devtool: 'eval',
-  target: 'node',
-  externals: [nodeExternals()],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader'
-        })
-      },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            {
-              loader: 'css-loader',
-              query: {
-                sourceMap: true
-              }
-            },
-            {
-              loader: 'sass-loader',
-              query: {
-                sourceMap: true
-              }
-            }
-          ]
-        })
-      },
-      {
-        test: /\.(jsx|js)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [['env', { modules: false }], 'react'],
-            plugins: [
-              'transform-runtime',
-              'transform-object-rest-spread',
-              'syntax-dynamic-import'
-            ],
-            babelrc: false
-          }
-        }
-      },
-      {
-        test: /\.(graphql|gql)$/,
-        exclude: /node_modules/,
-        loader: 'graphql-tag/loader'
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: 'images/[name].[ext]'
-        }
-      },
-      {
-        test: /\.(svg|eot|ttf|woff|woff2)$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: 'fonts/[name].[ext]'
-        }
-      }
-    ]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(config.NODE_ENV)
-    }),
-    new ExtractTextPlugin({
-      filename: 'css/styles.css',
-      disable: true
-    }),
-    new webpack.NamedModulesPlugin(),
-    new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }),
-    new webpack.NoEmitOnErrorsPlugin()
-  ],
-  resolve: {
-    extensions: [
-      '*',
-      '.json',
-      '.js',
-      '.jsx',
-      '.scss'
-    ]
-  }
-};
-
+  context: __dirname
+});
