7a9b05c9b5101f42ee052adb2665646badb0faaa
dmoiseenko <dmoiseenko@mykolab.com>
2017-06-02 14:05:06 +0300

    Add common webpack production config

---
+++ client/common/webpack/webpack.prod.config.js
@@ -0,0 +1,113 @@
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const AssetsPlugin = require('assets-webpack-plugin');
+
+const config = require('../../../common/config.js');
+
+
+module.exports = ({ entry, output, context }) => ({
+  entry,
+  output,
+  context,
+  devtool: 'hidden-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.(jsx|js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['env', { modules: false }], 'react'],
+            plugins: [
+              'transform-runtime',
+              'transform-object-rest-spread',
+              'syntax-dynamic-import'
+            ],
+            babelrc: false
+          }
+        }
+      },
+      {
+        test: /\.(graphql|gql)$/,
+        exclude: /node_modules/,
+        loader: 'graphql-tag/loader'
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'images/[name].[ext]'
+        }
+      },
+      {
+        test: /\.(svg|eot|ttf|woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'fonts/[name].[ext]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(config.NODE_ENV),
+      SOCKET_URL: JSON.stringify(config.socket.url)
+    }),
+    new ExtractTextPlugin({
+      filename: 'css/styles-[contenthash].css',
+      disable: false
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks(module) {
+        return module.context && module.context.indexOf('node_modules') !== -1;
+      }
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'manifest'
+    }),
+    new CompressionPlugin({
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.js$|\.css$|\.html$/
+    }),
+    new AssetsPlugin({
+      filename: 'assets.json',
+      path: output.path,
+      prettyPrint: true
+    }),
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+  resolve: {
+    extensions: [
+      '*',
+      '.json',
+      '.js',
+      '.jsx',
+      '.scss'
+    ],
+    alias: {
+      react: 'preact-compat',
+      'react-dom': 'preact-compat',
+      'create-react-class': 'preact-compat/lib/create-react-class'
+    }
+  }
+});
--- client/login/webpack/login.prod.webpack.config.js
+++ client/login/webpack/login.prod.webpack.config.js
@@ -1,121 +1,20 @@
-const webpack = require('webpack');
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const CompressionPlugin = require('compression-webpack-plugin');
-const AssetsPlugin = require('assets-webpack-plugin');
+
+const webpackConfig = require('../../common/webpack/webpack.prod.config.js');
 
 
-const outputPath = path.join(__dirname, '../../../public/login');
-
-module.exports = {
-  entry: {
-    app: [
-      '../Login.client.entry.jsx'
-    ]
-  },
-  output: {
-    path: outputPath,
-    publicPath: '/login/',
-    filename: 'js/[name]-[chunkhash].js'
-  },
-  context: __dirname,
-  devtool: 'hidden-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader'
-        })
-      },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
-        })
-      },
-      {
-        test: /\.(jsx|js)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [['env', { modules: false }], 'react'],
-            plugins: [
-              'transform-runtime',
-              'transform-object-rest-spread',
-              'syntax-dynamic-import'
-            ],
-            babelrc: false
-          }
-        }
-      },
-      {
-        test: /\.(graphql|gql)$/,
-        exclude: /node_modules/,
-        loader: 'graphql-tag/loader'
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: 'images/[name].[ext]'
-        }
-      },
-      {
-        test: /\.(svg|eot|ttf|woff|woff2)$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: 'fonts/[name].[ext]'
-        }
-      }
-    ]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    }),
-    new ExtractTextPlugin({
-      filename: 'css/styles-[contenthash].css',
-      disable: false
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks(module) {
-        return module.context && module.context.indexOf('node_modules') !== -1;
-      }
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'manifest'
-    }),
-    new CompressionPlugin({
-      asset: '[path].gz[query]',
-      algorithm: 'gzip',
-      test: /\.js$|\.css$|\.html$/
-    }),
-    new AssetsPlugin({
-      filename: 'assets.json',
-      path: outputPath,
-      prettyPrint: true
-    }),
-    new webpack.NoEmitOnErrorsPlugin()
-  ],
-  resolve: {
-    extensions: [
-      '*',
-      '.json',
-      '.js',
-      '.jsx',
-      '.scss'
-    ],
-    alias: {
-      react: 'preact-compat',
-      'react-dom': 'preact-compat',
-      'create-react-class': 'preact-compat/lib/create-react-class'
-    }
+module.exports = webpackConfig(
+  {
+    entry: {
+      app: [
+        '../Login.client.entry.jsx'
+      ]
+    },
+    output: {
+      path: path.join(__dirname, '../../../public/login'),
+      publicPath: '/login/',
+      filename: 'js/[name]-[chunkhash].js'
+    },
+    context: __dirname
   }
-};
+);
